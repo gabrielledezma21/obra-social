b144926826c5ab0dd859705709385146b187c190
Gabriel Agustin Ledezma <165868657+gabrielledezma21@users.noreply.github.com>
2026-08-12 06:14:52 -0300

    fix: validate and persist address updates

--- src/services/direccionService.js
+++ src/services/direccionService.js
@@ -2,51 +2,31 @@ const { Direccion } = require("../models");
 const AppError = require("../exceptions/appError");
 const { capitalizarCadena } = require("../utils");
 
-const createDireccion = async (data) => {
-    try {
-        const d = data || {};
-        const direccion = await Direccion.create(
-            [{
-                calle: d.calle ? await capitalizarCadena(d.calle) : undefined,
-                altura: d.altura,
-                pisoDepto: d.pisoDepto || null,
-                localidad: d.localidad ? await capitalizarCadena(d.localidad) : undefined,
-                codigoPostal: d.codigoPostal,
-                provincia: d.provincia ? validarProvincia(await capitalizarCadena(d.provincia)) : undefined,
-            }]
-        );
-
-        return direccion[0];
-
-    } catch (error) {
-        throw new AppError(error.message, error.statusCode);
-    }
+const provincias = ['Buenos Aires', 'Ciudad Autónoma de Buenos Aires', 'Catamarca', 'Chaco', 'Chubut', 'Córdoba', 'Corrientes', 'Entre Ríos', 'Formosa', 'Jujuy', 'La Pampa', 'La Rioja', 'Mendoza', 'Misiones', 'Neuquén', 'Río Negro', 'Salta', 'San Juan', 'San Luis', 'Santa Cruz', 'Santa Fe', 'Santiago del Estero', 'Tierra del Fuego', 'Tucumán'];
+const validarProvincia = (provincia) => {
+  if (!provincias.includes(provincia)) throw new AppError('Provincia inválida', 400, 'PROVINCIA_INVALIDA');
+  return provincia;
 };
 
-const validarProvincia = (provincia) => {
-    const provincias = ['Buenos Aires', 'Ciudad Autónoma de Buenos Aires', 'Catamarca', 'Chaco', 'Chubut', 'Córdoba', 'Corrientes', 'Entre Ríos', 'Formosa', 'Jujuy', 'La Pampa', 'La Rioja', 'Mendoza', 'Misiones', 'Neuquén', 'Río Negro', 'Salta', 'San Juan', 'San Luis', 'Santa Cruz', 'Santa Fe', 'Santiago del Estero', 'Tierra del Fuego', 'Tucumán'];
-    if (!provincias.includes(provincia)) {
-        throw new AppError('Provincia inválida', 400, 'PROVINCIA_INVALIDA');
-    }
-    return provincia;
-}
+const createDireccion = async (data = {}) => Direccion.create({
+  calle: data.calle ? await capitalizarCadena(data.calle) : undefined,
+  altura: data.altura, pisoDepto: data.pisoDepto || null,
+  localidad: data.localidad ? await capitalizarCadena(data.localidad) : undefined,
+  codigoPostal: data.codigoPostal,
+  provincia: data.provincia ? validarProvincia(await capitalizarCadena(data.provincia)) : undefined,
+});
 
-const updateDireccion = async (id, data) => {
-    try {
-        const d = data || {};
-        const updateData = {};
-        if (d.calle) updateData.calle = await capitalizarCadena(d.calle);
-        if (d.altura) updateData.altura = d.altura;
-        if (d.pisoDepto !== undefined) updateData.pisoDepto = d.pisoDepto;
-        if (d.localidad) updateData.localidad = await capitalizarCadena(d.localidad);
-        if (d.codigoPostal) updateData.codigoPostal = d.codigoPostal;
-        if (d.provincia) updateData.provincia = validarProvincia(await capitalizarCadena(d.provincia));
-
-        const direccion = await Direccion.findByIdAndUpdate(id, updateData, { new: true });
-        return direccion;
-    } catch (error) {
-        throw new AppError(error.message, error.statusCode);
-    }
-}
+const updateDireccion = async (id, data = {}) => {
+  const updateData = {};
+  if (data.calle !== undefined) updateData.calle = await capitalizarCadena(data.calle);
+  if (data.altura !== undefined) updateData.altura = data.altura;
+  if (data.pisoDepto !== undefined) updateData.pisoDepto = data.pisoDepto || null;
+  if (data.localidad !== undefined) updateData.localidad = await capitalizarCadena(data.localidad);
+  if (data.codigoPostal !== undefined) updateData.codigoPostal = data.codigoPostal;
+  if (data.provincia !== undefined) updateData.provincia = validarProvincia(await capitalizarCadena(data.provincia));
+  const direccion = await Direccion.findByIdAndUpdate(id, updateData, { new: true, runValidators: true });
+  if (!direccion) throw new AppError('Dirección no encontrada', 404, 'DIRECCION_NO_ENCONTRADA');
+  return direccion;
+};
 
 module.exports = { createDireccion, updateDireccion };
