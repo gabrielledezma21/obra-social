60c3c3250365b1933ec6d910184e80aa092de842
Gabriel Agustin Ledezma <165868657+gabrielledezma21@users.noreply.github.com>
2026-08-12 06:16:09 -0300

    fix: validate provider updates and rollback failed creates

--- src/services/prestadorService.js
+++ src/services/prestadorService.js
@@ -1,47 +1,30 @@
 const { Prestador } = require("../models");
 const centroDeAtencionService = require("./centroDeAtencionService");
 const AppError = require("../exceptions/appError");
-const { mongo } = require("../config/");
 
 const createPrestador = async (data) => {
-    try {
-        // 1. Crear centros de atención (esperando resultados)
-        const centrosDeAtencion = await Promise.all(
-            (data.centrosDeAtencion || []).map((centro) =>
-                centroDeAtencionService.createCentroDeAtencion(centro)
-            )
-        );
-
-        // 2. Crear prestador
-        const prestador = await Prestador.create(
-            [{
-                nombre: data.nombre,
-                cuilCuit: data.cuilCuit,
-                emails: data.emails,
-                telefonos: data.telefonos,
-                especialidades: data.especialidades,
-                centrosDeAtencion: centrosDeAtencion.map(c => c._id),
-                esCentroMedico: data.esCentroMedico,
-                centroMedicoQueIntegra: data.centroMedicoQueIntegra,
-            }]
-        );
-
-        return prestador[0];
-
-    } catch (error) {
-        throw new AppError(error.message, error.statusCode);
-    }
+  const centrosDeAtencion = await Promise.all(
+    (data.centrosDeAtencion || []).map((centro) => centroDeAtencionService.createCentroDeAtencion(centro))
+  );
+  try {
+    return await Prestador.create({
+      nombre: data.nombre, cuilCuit: data.cuilCuit, emails: data.emails || [],
+      telefonos: data.telefonos || [], especialidades: data.especialidades || [],
+      centrosDeAtencion: centrosDeAtencion.map((c) => c._id),
+      esCentroMedico: data.esCentroMedico, centroMedicoQueIntegra: data.centroMedicoQueIntegra || null
+    });
+  } catch (error) {
+    await centroDeAtencionService.deleteCentrosDeAtencion(centrosDeAtencion.map((c) => c._id)).catch(() => {});
+    throw error;
+  }
 };
 
 const updatePrestador = async (id, data) => {
-    try {
-        const prestador = await Prestador.findByIdAndUpdate(id, data, { new: true });
-        return prestador;
-    } catch (error) {
-        throw new AppError(error.message, error.statusCode);
-    }
+  const update = { ...data };
+  delete update.centrosDeAtencion;
+  const prestador = await Prestador.findByIdAndUpdate(id, update, { new: true, runValidators: true });
+  if (!prestador) throw new AppError('Prestador no encontrado', 404, 'PRESTADOR_NO_ENCONTRADO');
+  return prestador;
 };
 
 module.exports = { createPrestador, updatePrestador };
-
-
