81ff7910571d31cdf5c633e99fd8d67ae2dbb18d
Gabriel Agustin Ledezma <165868657+gabrielledezma21@users.noreply.github.com>
2026-08-19 19:04:38 -0300

    feat: priorizar flujo centralizado de turnos del afiliado

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ const disponibilidadTurnosRutas = require('./routes/disponibilidadTurnosRutas');
 const portalAfiliadoRutas = require('./routes/portalAfiliadoRutas');
 const portalPrestadorRutas = require('./routes/portalPrestadorRutas');
 const reporteRutas = require('./routes/reporteRutas');
+const turnosAfiliadoRutas = require('./routes/turnosAfiliadoRutas');
 const turnosPublicosRutas = require('./routes/turnosPublicosRutas');
 const { logRequest: registrarPeticion } = require('./middlewares/genericMiddleware');
 const {
@@ -117,6 +118,11 @@ const configurarAplicacion = (aplicacion) => {
   );
   aplicacion.use('/reportes', ...protegerAdministracion, reporteRutas);
 
+  aplicacion.use(
+    '/portal-afiliado',
+    ...protegerPortalAfiliado,
+    turnosAfiliadoRutas
+  );
   aplicacion.use(
     '/portal-afiliado',
     ...protegerPortalAfiliado,
